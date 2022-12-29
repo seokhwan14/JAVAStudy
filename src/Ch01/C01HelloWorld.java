e9863b40b0b65780ac243e384d40a7b34824e32f
xcapmaxx <xcapmaxx@hanmail.net>
2022-12-29 17:06:48 +0900

    Ch02

---
+++ src/Ch01/C01HelloWorld.java
@@ -0,0 +1,22 @@
+package Ch01;	// 패키지명
+
+/*
+ * 클래스명 - 객체지향 문법 적용단위 
+ */
+public class C01HelloWorld {		
+	
+	/*
+	 * main - 메서드 영역, 절차 지향 문법 적용 단위
+	 * 
+	 * 메서드(방법,기능) : 작업수행을 하는데 필요한 여러코드를 묶어서 실행하는데 사용되는 단위
+	 * main 메서드 : 최초 실행 메서드
+	 * system library 메서드 : 미리 만들어져서 제공되는 메서드
+	 * 사용자 정의 메서드 : 내가 직접 만들어서 사용하는 메서드
+	 */
+	public static void main(String[] args) {	
+		
+		System.out.println("HELLO WORLD");
+		
+	}
+
+}
